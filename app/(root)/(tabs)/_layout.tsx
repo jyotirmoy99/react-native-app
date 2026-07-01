b5f3748471223675dbd4926281a02fe0d9437bb5
jyotirmoy99 <jyotirmoy@connectingpointstech.com>
2026-07-01 22:37:16 +0530

    feat: implement tab navigation with admin-specific create option and add property type definition

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,6 +1,69 @@
+import { userUserStore } from "@/store/userStore";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { Platform } from "react-native";
 
-export default function TabLayout() {
+function AndroidTabs() {
+  const isAdmin = userUserStore((state) => state.isAdmin);
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Add",
+          href: isAdmin ? undefined : null,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+function IOSTabs() {
+  const isAdmin = userUserStore((state) => state.isAdmin);
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
@@ -14,6 +77,12 @@ export default function TabLayout() {
       </NativeTabs.Trigger>
 
       {/* Create Property */}
+      {isAdmin && (
+        <NativeTabs.Trigger name="create">
+          <Icon sf="plus.circle.fill" />
+          <Label>Add property</Label>
+        </NativeTabs.Trigger>
+      )}
 
       <NativeTabs.Trigger name="saved">
         <Label>Saved</Label>
@@ -26,4 +95,9 @@ export default function TabLayout() {
       </NativeTabs.Trigger>
     </NativeTabs>
   );
+}
+
+export default function TabsLayout() {
+  return Platform.OS === "ios" ? <IOSTabs /> : <AndroidTabs />;
+  // return <AndroidTabs />;
 }
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,13 +1,89 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { supabase } from "@/lib/supabase";
+import { Property } from "@/types";
+import { useUser } from "@clerk/expo";
+import { useFocusEffect, useRouter } from "expo-router";
+import { useCallback, useState } from "react";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const [featured, setFeatured] = useState<Property[]>([]);
+  const [recommended, setRecommended] = useState<Property[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  // console.log(featured, recommended);
+
+  const fetchProperties = async () => {
+    setLoading(true);
+
+    try {
+      const { data: featuredData, error: featuredError } = await supabase
+        .from("properties")
+        .select("*")
+        .eq("is_featured", true)
+        .order("created_at", { ascending: false });
+
+      if (featuredError) {
+        throw featuredError;
+      }
+
+      const { data: recommendedData, error: recommendedError } = await supabase
+        .from("properties")
+        .select("*")
+        .eq("is_featured", false)
+        .order("created_at", { ascending: false });
+
+      if (recommendedError) {
+        throw recommendedError;
+      }
+
+      setFeatured(featuredData ?? []);
+      setRecommended(recommendedData ?? []);
+    } catch (error) {
+      console.error("Error fetching properties:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchProperties();
+    }, []),
+  );
+
   return (
     <SafeAreaView className="flex-1 bg-gray-50">
-      <View>
-        <Text>HomeScreen</Text>
-      </View>
+      <FlatList
+        data={recommended}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsHorizontalScrollIndicator={false}
+        ListHeaderComponent={
+          <View>
+            {/* Header */}
+
+            {/* Search bar */}
+
+            {/* Featured Section */}
+
+            {/* Recommended Header */}
+            <Text className="text-gray-900 text-lg font-bold px-5 mb-4">
+              Recommended
+              {/* 1:47:30 */}
+            </Text>
+          </View>
+        }
+
+        renderItem={({ item }) => (
+          <View className="px-5">
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 }
